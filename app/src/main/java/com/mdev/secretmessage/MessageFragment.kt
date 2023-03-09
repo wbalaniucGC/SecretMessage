ca51a8126b0340db9d22568973626eb03642244c
Wallace Balaniuc <wallace.balaniuc@gmail.com>
2023-03-09 12:46:35 -0500

    Week 8 Content

--- app/src/main/java/com/mdev/secretmessage/MessageFragment.kt
+++ app/src/main/java/com/mdev/secretmessage/MessageFragment.kt
@@ -5,12 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.findNavController
 
 class MessageFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_message, container, false)
+        val nextButton = view.findViewById<Button>(R.id.next)
+        val messageView = view.findViewById<EditText>(R.id.message)
 
+        nextButton.setOnClickListener {
+            val message = messageView.text.toString()
+            val action = MessageFragmentDirections.actionMessageFragmentToEncryptFragment(message)
+            view.findNavController().navigate(action)
+            // view.findNavController().navigate(R.id.action_messageFragment_to_encryptFragment)
+        }
+
+        return view
+    }
 }
